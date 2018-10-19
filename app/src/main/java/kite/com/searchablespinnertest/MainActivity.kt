db5ab2dc43cf0f757542c33f480866cd6b9a0a89
DESKTOP-MFIIQC5\meste <mester@kite.hu>
2018-10-19 09:05:02 +0200

    Initial commit

---
+++ app/src/main/java/kite/com/searchablespinnertest/MainActivity.kt
@@ -0,0 +1,12 @@
+package kite.com.searchablespinnertest
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
